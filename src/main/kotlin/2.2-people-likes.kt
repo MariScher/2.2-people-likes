34e1f0f3277cef02831ec7c5b8959fb9a785c28c
MariScher <marina_vetr@mail.ru>
2022-02-27 20:59:41 +0300

    Changes

--- src/main/kotlin/2.2-people-likes.kt
+++ src/main/kotlin/2.2-people-likes.kt
@@ -1,8 +1,8 @@
 fun main() {
-    val likes = 35
+    val likes = 111
     val singular = "человеку"
     val plural = "людям"
 
-    val people = if (likes % 10 == 1 && likes != 11) singular else plural
+    val people = if (likes % 10 == 1 && (likes - 11) % 100 != 0) singular else plural
     println("Понравилось $likes $people")
 }
